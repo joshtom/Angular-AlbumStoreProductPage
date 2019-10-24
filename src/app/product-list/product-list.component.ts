18ed610ef739e990ee4dbd9b46ecdb639e223e29
joshtom <olajidejoshua4real@gmail.com>
2019-10-24 12:17:40 +0100

    Fixed the response error on the getProducts Method

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -10,8 +10,9 @@ import { ProductService } from '../product.service';
 export class ProductListComponent implements OnInit {
   products: Product[]
   constructor(private _productService: ProductService) { }
-  
+
   ngOnInit() {
+    this._productService.getProducts().subscribe(response => this.products = response);
   }
 
 }
--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -17,5 +17,8 @@ export class ProductService {
     <Album>response.json());
     
    }
-   getProducts() {};
+   getProducts(): Observable<Product[]>{
+     return this._http.get(this._productsUrl).map((response) => 
+      <Product[]>response.json());
+   };
 }
